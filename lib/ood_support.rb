4feecc6cd5a570f1df8cf4f94fc40ba05d42d7f5
Jeremy Nicklas <jnicklas@osc.edu>
2016-07-07 11:47:29 -0400

    add posix acl support

--- lib/ood_support.rb
+++ lib/ood_support.rb
@@ -11,5 +11,6 @@ module OodSupport
   # A namespace to hold all subclasses of {ACL} and {ACLEntry}
   module ACLs
     require 'ood_support/acls/nfs4'
+    require 'ood_support/acls/posix'
   end
 end
